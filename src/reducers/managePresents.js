0f1fd336906cfc7ce9367794a78e992f83cf6062
Evan <evanfujita@gmail.com>
2021-03-04 08:41:33 -0600

    commit tree 4062bb0edcf815aa1776b73d5f2964c0dccb8dd1

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,15 @@
 export function managePresents(state, action){
 }
+
+
+managePresents = (prevState, action) => {
+    switch(action.type) {
+        case 'INCREASE':
+            return {numberOfPresents: prevState.numberOfPresents + 1}
+
+        default: 
+            return prevState
+    }
+    
+
+}
